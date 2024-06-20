eef1b4424f4130ef7e660e43d3330f0dbb3b904f
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-06-20 13:09:12 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 import pickle
 import numpy as np
-pip install plotly
+pip install -r requirements.txt
 import plotly.express as px
 
 model = pickle.load(open('rf_model.pkl','rb'))
