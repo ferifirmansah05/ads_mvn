eebc16d88b64aea46eb1c69c6b9d7ff25444bc5f
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-06-20 15:41:33 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -5,9 +5,6 @@ from datetime import datetime, timedelta
 import pickle
 import numpy as np
 import subprocess
-from io import BytesIO
-
-    
 import plotly.express as px
 import requests
 import os
@@ -42,7 +39,7 @@ def read(file_path):
         print(f"Failed to load model: {e}")
         return None
 
-url = 'https://raw.githubusercontent.com/ferifirmansah05/ads_mvn/main/rf_model.pkl'
+url = 'https://github.com/ferifirmansah05/ads_mvn/edit/main/rf_model.pkl'
 save_path = 'rf_model.pkl'
 download_file_from_github(url, save_path)
 if os.path.exists(save_path):
