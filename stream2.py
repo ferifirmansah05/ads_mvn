535206800514842348e706b9f80dbbf1702ecf58
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-06-27 09:12:31 +0700

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -2,14 +2,14 @@ import requests
 import subprocess
 
 # URL file GitHub
-url = 'https://raw.githubusercontent.com/username/repository/branch/filename.py'
+url = 'https://raw.githubusercontent.com/ferifirmansah05/ads_mvn/main/stream.py'
 
 # Download file dari GitHub
 response = requests.get(url)
 
 # Simpan file ke direktori lokal
-with open('filename.py', 'wb') as file:
+with open('stream.py', 'wb') as file:
     file.write(response.content)
 
 # Jalankan file yang telah didownload
-subprocess.run(['python', 'filename.py'])
+subprocess.run(['python', 'stream.py'])
