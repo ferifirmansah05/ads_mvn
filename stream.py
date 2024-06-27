205eb92e8040450a2bf07369c0cb5ee65d94817b
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-06-27 13:26:44 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -39,7 +39,7 @@ def list_files_in_directory(dir_path):
         for file_name in files:
             st.write(f'  - {file_name}')
             
-st.write(list_files_in_directory('tmp'))
+st.write(list_files_in_directory('tmp/'))
 
 # URL file model .pkl di GitHub (gunakan URL raw dari file .pkl di GitHub)
 url = 'https://raw.githubusercontent.com/ferifirmansah05/ads_mvn/main/database provinsi.xlsx'
