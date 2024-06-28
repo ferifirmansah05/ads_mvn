1254c13fd9f0d37556b10147104acbf5a5aa2999
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-06-28 08:22:34 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -582,7 +582,7 @@ if uploaded_file is not None:
                             df = html_file[0].iloc[8:]  # Remove the first row
                             dataframes.append(df)
                     except Exception as e:
-                        print(f"Error reading {file_path}: {e}")
+                        st,write(f"Error reading {file_path}: {e}")
             
             # Check if any HTML files were processed
             if dataframes:
