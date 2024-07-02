b63f6e65d15e41ecf0f77f9106eabe7052c3cd8c
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-07-02 11:52:29 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -1788,8 +1788,6 @@ if uploaded_file is not None:
                         df_all3.loc[:,'HELP'] = ''
                         for i in df_all3[(df_all3['HELP']=='')].index:
                             if (df_all3.loc[i,'SOURCE']=='WEB') & (df_all3.loc[i,'HELP']==''):
-                                st.write(df_all3)
-                                st.write(pd.to_datetime(df_all3.loc[i,'DATE'])+ dt.timedelta(days=1))
                                 x = df_all3[(df_all3['DATE']==(pd.to_datetime(df_all3.loc[i,'DATE'])+ dt.timedelta(days=1)).strftime('%Y-%m-%d')) 
                                         & (abs(df_all3.loc[i,'NOM'] - df_all3['NOM']) <=200)
                                         & (df_all3['SOURCE']=='INVOICE')].index
