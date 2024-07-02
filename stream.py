a99894c08b1001b77b53282acb0136a03a0da92d
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-07-02 16:55:44 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -1822,23 +1822,23 @@ if uploaded_file is not None:
             
             #combined_dataframes.append(df_all)
             final_df = pd.concat(df_concat)
-            st.markdown('### Output')
+st.markdown('### Output')
         
             
-            st.download_button(
+st.download_button(
                 label="Download File Invoice",
                 data=invoice_final.to_csv(index=False),
                 file_name=f'INVOICE_{dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv',
                 mime='text/csv',
             )
-            st.download_button(
+st.download_button(
                 label="Download File Web",
                 data=web_final.to_csv(index=False),
                 file_name=f'WEB_{dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv',
                 mime='text/csv',
             )
             
-            st.download_button(
+st.download_button(
                 label="Download File Breakdownn",
                 data=final_df.to_csv(index=False),
                 file_name=f'BREAKDOWN_{dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv',
