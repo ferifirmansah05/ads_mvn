0d288c348717086f18638523020287da736b2830
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-10-29 06:58:35 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -791,6 +791,7 @@ if uploaded_file is not None:
                             
                 dfweb['TIME'] = dfweb['TIME'].apply(convert_time)
                 st.write(dfweb)
+                dfweb['DISC'] = dfweb['DISC'].fillna(0)
                 dfweb['NOM'] = dfweb.apply(lambda row: float(row['NOM2'])+float(row['DISC']) if (row['NOM2'].isnumeric()) else '',axis=1)
                 dfweb = dfweb.drop(columns='DISC')
 
