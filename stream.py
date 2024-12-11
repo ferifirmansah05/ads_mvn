2fa5b0e36549fec7299ea79f77e8d88517c4beda
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-11 09:50:45 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -1206,8 +1206,8 @@ if uploaded_file is not None:
                         gfi.drop_duplicates(inplace=True)
             
                         gfw.loc[gfw[gfw['ID'].isna()].index,'ID'] = ''
-                        gfw['ID2'] = gfw['ID'].apply(lambda x: re.findall(r'\d+', x)[-1] if re.findall(r'\d+', x) else 0)
-                        gfi['ID2'] = gfi['ID'].apply(lambda x: re.findall(r'\d+', x)[-1] if re.findall(r'\d+', x) else 0)
+                        gfw['ID2'] = gfw['ID'].apply(lambda x: str(re.findall(r'\d+', x)[-1]) if re.findall(r'\d+', x) else 0)
+                        gfi['ID2'] = gfi['ID'].apply(lambda x: str(re.findall(r'\d+', x)[-1]) if re.findall(r'\d+', x) else 0)
             
                         gfw.loc[gfw[gfw['ID'].isna()].index,'ID'] = ''
                         for i in cn[(cn['TANGGAL']==str(int(re.findall(r'\d+', date)[-1]))) & (cn['CAB']==cab) & (cn['TYPE BAYAR']=='GRAB FOOD')].index:
@@ -1248,7 +1248,19 @@ if uploaded_file is not None:
                                                         df_i.loc[x,'KET'] = 'Promo Marketing/Adjustment'
                                                         df_i.loc[x,'HELP'] = df_w.loc[i,'CODE']
                                                         break 
-            
+                                                        
+                            for x in df_i[(df_i['ID'].apply(lambda x: len(re.findall(r'\bGF-\d{3}\b', x)))>=2) & (df_i['HELP']=='')].index:
+                                print([str(x) for x in df_i.loc[x,'KET'].replace('GF-','').split(', ')])
+                                if len(df_w[df_w['ID2'].isin([str(x) for x in df_i.loc[x,'KET'].replace('GF-','').split(', ')])]) >=2:
+                                    if abs(df_i.loc[x,'NOM'] - df_w[df_w['ID2'].isin([str(x) for x in df_i.loc[x,'KET'].replace('GF-','').split(', ')])]['NOM2'].astype(float).sum())< 5:
+                                        df_w.loc[df_w[df_w['ID2'].isin([str(x) for x in df_i.loc[x,'KET'].replace('GF-','').split(', ')])].index[0],'KET'] = 'Selisih IT'
+                                        df_w.loc[df_w[df_w['ID2'].isin([str(x) for x in df_i.loc[x,'KET'].replace('GF-','').split(', ')])].index[1],'KET'] = 'Selisih IT'
+                                        df_w.loc[df_w[df_w['ID2'].isin([str(x) for x in df_i.loc[x,'KET'].replace('GF-','').split(', ')])].index[0],'ID2'] = df_i.loc[x,'KET'].replace('GF-','')
+                                        df_w.loc[df_w[df_w['ID2'].isin([str(x) for x in df_i.loc[x,'KET'].replace('GF-','').split(', ')])].index,'ID2'] = df_i.loc[x,'KET'].replace('GF-','')
+                                        df_i.loc[x,'HELP'] = df_i.loc[x,'KET'].replace('GF-','')
+                                        df_i.loc[x,'ID2'] = df_i.loc[x,'KET'].replace('GF-','')
+                                        df_i.loc[x,'KET'] = 'Selisih IT'
+                                        
                             for i in df_w[df_w['KET']==''].index :
                                 if df_w.loc[i,'TIME'] > pd.to_datetime('23:00:00' , format='%H:%M:%S'):
                                     df_w.loc[i,'KET'] = 'Invoice Beda Hari'
