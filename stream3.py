0be3d53f4646d59bbd6638175bba3398033e5f65
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-16 10:59:27 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -18,23 +18,6 @@ gb.configure_default_column(editable=True, resizable=True)
 gb.configure_column("Nama", pinned="left")  # Membekukan kolom pertama
 gb.configure_grid_options(domLayout='autoHeight') 
 grid_options = gb.build()
-st.markdown("""
-    <style>
-        .stAgGrid {
-            margin: 0;  /* Hilangkan margin */
-            padding: 0; /* Hilangkan padding */
-        }
-        .ag-root-wrapper {
-            border: none !important; /* Hilangkan border luar */
-        }
-        .ag-header {
-            border-bottom: none !important; /* Opsional: Hilangkan border bawah header */
-        }
-        .ag-row {
-            border-bottom: none !important; /* Opsional: Hilangkan garis antar baris */
-        }
-    </style>
-    """, unsafe_allow_html=True)
 st.write("Tabel dengan kolom pertama dibekukan:")
 AgGrid(df, gridOptions=grid_options,fit_columns_on_grid_load=True, height=None)
 
