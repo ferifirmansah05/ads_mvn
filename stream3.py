f513aaa8b573d7455df30bdd8c5a75b0cbfda13f
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-16 15:06:52 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -12,6 +12,6 @@ df = pd.DataFrame(data)
 
 # Create grid options
 gb = GridOptionsBuilder.from_dataframe(df)
-
+st.dataframe(df.style.background_gradient(cmap='Reds', axis=1, subset=df.columns[1:]))
 # Render the grid with the styled DataFrame
 AgGrid(df.style.background_gradient(cmap='Reds', axis=1, subset=df.columns[1:]), gridOptions=gb.build())
