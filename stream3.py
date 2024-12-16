802468d34305bca85a441446d200a9031c7aa573
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-16 15:08:43 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder
 import streamlit as st
+import matplotlib.pyplot as plt
 
 # Sample data with numeric columns for gradient application
 data = {
