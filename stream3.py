e46e45f9c7abe1cd666eb54a1b08b904595234c1
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-18 08:38:14 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -43,7 +43,7 @@ row_colors = df.iloc[:, 1:].apply(lambda row: row_gradient_colors(row, cmap), ax
 
 # 6. Konfigurasi AgGrid
 gb = GridOptionsBuilder.from_dataframe(df)
-gb.configure_default_column(cellStyle={'color': 'black', 'font-size': '12px'}, suppressMenu=True, wrapHeaderText=True, autoHeaderHeight=True)
+
 
 # Menambahkan cellStyle untuk setiap kolom numerik
 for col_idx, col in enumerate(df.columns[1:]):
