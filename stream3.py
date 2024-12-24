2451b236a8b20dccdc2993b49c05e42a6410be04
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-24 09:00:53 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1649,7 +1649,7 @@ if uploaded_file is not None:
                                         & (df_all3['ID2'] == df_all3.loc[i,'ID2'])
                                         & (abs(df_all3.loc[i,'NOM'] - df_all3['NOM']) <=200)
                                         & (df_all3['SOURCE']=='INVOICE') & (df_all3['HELP']=='')
-                                        & (abs(pd.to_datetime(str(pd.to_datetime(df_all3.loc[i,'TIME']) - pd.to_datetime(df_all3['TIME'])))) <= dt.timedelta(minutes=150))].index
+                                        & (abs(pd.to_datetime(pd.to_datetime(df_all3.loc[i,'TIME']) - pd.to_datetime(df_all3['TIME']))) <= dt.timedelta(minutes=150))].index
                                 if kat in ['GO RESTO', 'QRIS SHOPEE', 'QRIS TELKOM']:
                                     x = df_all3[(df_all3['DATE']==(pd.to_datetime(df_all3.loc[i,'DATE'])+ dt.timedelta(days=1)).strftime('%Y-%m-%d')) 
                                         & (abs(df_all3.loc[i,'NOM'] - df_all3['NOM']) <=200)
