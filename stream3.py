99877a6b90b0e7099cc76a168a02f9936946afa5
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-30 11:52:14 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1661,6 +1661,7 @@ if uploaded_file is not None:
                     # Concatenate CSV files within each subfolder
             df_all = pd.concat(files, ignore_index=True)
             df_concat = []
+            df_all['NOTE'] = ''
             for cab in all_cab:
                 for kat in ['GO RESTO', 'QRIS SHOPEE', 'GRAB FOOD','SHOPEEPAY', 'QRIS ESB','QRIS TELKOM','EDC']:
                     if not df_all[(df_all['CAB'] == cab) & (df_all['KAT'].str.contains(kat))].empty:
