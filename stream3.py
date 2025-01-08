2ef983b31cc136d842eb2fa9cc2cc5ee91e7b84f
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-08 10:43:56 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1775,7 +1775,7 @@ if uploaded_file is not None:
                 final_df =  final_df[['CAB','DATE','TIME','CODE','ID','NOM','KAT','SOURCE','KET','HELP','ID2','NOTE']]
             final_df['TIME'] = pd.to_datetime(final_df['TIME']).dt.strftime('%H:%M:%S')
             final_df['KET'] = final_df['KET'].str.replace('+-','-')
-            final_df
+        
             time_now = dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
             st.markdown('### Output')
             zip_buffer = io.BytesIO()
