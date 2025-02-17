68e65351a2a0da991e48f9983188747e0038b6bc
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-02-17 10:13:31 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -858,7 +858,7 @@ if uploaded_file is not None:
             dfweb['TIME'] = pd.to_datetime(dfweb['DATE'].dt.strftime('%Y-%m-%d') + ' ' + dfweb['TIME'])
             dfweb['TIME2'] = pd.to_datetime(dfweb['DATE'].dt.strftime('%Y-%m-%d') + ' ' + dfweb['TIME2'])
             dfinv['TIME'] = pd.to_datetime(dfinv['DATE'].dt.strftime('%Y-%m-%d') + ' ' + dfinv['TIME'].astype(str))
-            
+            dfweb['CODE'] = dfweb['CODE'].astype(str)
             dfweb['CODE2'] = dfweb['CODE'].apply(lambda x: x[-9:] if 'S' in x else x[6:]).astype(int)
             dfweb = dfweb.sort_values(['CAB','DATE','CODE2']).reset_index(drop=True)
             
