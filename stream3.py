334f83d50847abac8d95d2cf53dcc58a1dad9774
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-03-03 11:02:15 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -834,7 +834,7 @@ if uploaded_file is not None:
                 dfweb['TIME2'] = pd.to_datetime(pd.to_datetime(dfweb['TIME2'], errors='coerce').fillna(pd.to_datetime(dfweb['DATE']).dt.strftime('%Y-%m-%d')+ ' ' + dfweb['TIME2'].astype(str)).astype(str))
                 #dfweb['TIME'] = dfweb['TIME'].apply(convert_time)
                 dfweb['DISC'] = dfweb['DISC'].replace('',0).fillna(0)
-                #st.write(dfweb)
+                st.write(dfweb)
                 dfweb['NOM'] = dfweb['NOM2'].astype(float)+dfweb['DISC'].astype(float)
                 dfweb = dfweb.drop(columns='DISC')
 
@@ -882,8 +882,8 @@ if uploaded_file is not None:
             #dfweb['CODE'] = dfweb['CODE'].astype(str)
             dfweb['CODE2'] = dfweb['CODE'].apply(lambda x: x[-9:] if 'S' in x else x[6:]).astype(int)
             dfweb = dfweb.sort_values(['CAB','DATE','CODE2']).reset_index(drop=True)
-            #cab_time
-            #dfweb
+            cab_time
+            dfweb
             def adjust_date(row, previous_row):
                 if previous_row is not None and abs(row['TIME2'] - previous_row['TIME2']) > pd.Timedelta(hours=12):
                     return index
@@ -902,7 +902,7 @@ if uploaded_file is not None:
                                 break
                             previous_row = row
                         previous_row = None
-            #dfweb
+            dfweb
             dfweb = dfweb.drop(columns='TIME2')
             
             
