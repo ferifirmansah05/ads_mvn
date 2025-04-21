548c96db1ae99c8990a8897780c3e21a5501d655
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-04-21 11:07:55 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -855,7 +855,7 @@ if uploaded_file is not None:
                 dfweb = dfweb.drop(columns='DISC')
 
                 dfweb['KAT'] = dfweb['KAT'].replace({'SHOPEE PAY': 'SHOPEEPAY', 'SHOPEEFOOD INT': 'SHOPEEPAY','SHOPEE FOOD INT':'SHOPEEPAY', 
-                                                     'GORESTO': 'GO RESTO','GOFOOD':'GO RESTO','GOFOOD INT':'GO RESTO' ,'GRAB': 'GRAB FOOD','GRAB FOOD INT':'GRAB FOOD', 
+                                                     'GORESTO': 'GO RESTO','GOFOOD':'GO RESTO','GOFOOD INT':'GO RESTO' ,'GRAB': 'GRAB FOOD','GRAB FOOD INT':'GRAB FOOD','GRABFOOD INT':'GRAB FOOD', 
                                                      'QRIS ESB ORDER':'QRIS ESB', 'EDC CIMB':'EDC'})
             
             dfinv = pd.concat(dfinv, ignore_index = True).fillna('')
